bd3332ec0810037628816a26b0462bcc0b93d1d4
oharakogakuin <ohara@g.neec.ac.jp>
2022-10-04 10:02:24 +0900

    01_02.配布プロジェクトについて

--- GameScene.cpp
+++ GameScene.cpp
@@ -5,6 +5,10 @@ using namespace DirectX;
 
 GameScene::GameScene()
 {
+	// スプライトの解放
+	delete sprite1;
+	delete sprite2;
+
 }
 
 GameScene::~GameScene()
@@ -35,6 +39,15 @@ void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
 	// 3Dオブジェクト生成
 	object3d = Object3d::Create();
 	object3d->Update();
+
+	// テクスチャ2番に読み込み
+	Sprite::LoadTexture(2, L"Resources/texture.png");
+
+	// 座標{ 0, 0 }に、テクスチャ2番のスプライトを生成
+	sprite1 = Sprite::Create(2, { 0, 0 });
+	// 座標{ 500, 500 }に、テクスチャ2番のスプライトを生成
+	sprite2 = Sprite::Create(2, { 500, 500 }, { 1,0,0,1 }, { 0,0 }, false, true);
+
 }
 
 void GameScene::Update()
@@ -65,6 +78,18 @@ void GameScene::Update()
 	}
 
 	object3d->Update();
+
+
+	// スペースキーを押していたら
+	if (input->PushKey(DIK_SPACE)) {
+		// 現在の座標を取得
+		XMFLOAT2 position = sprite1->GetPosition();
+		// 移動後の座標を計算
+		position.x += 1.0f;
+		// 座標の変更を反映
+		sprite1->SetPosition(position);
+	}
+
 }
 
 void GameScene::Draw()
@@ -110,6 +135,10 @@ void GameScene::Draw()
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+
+	// 描画
+	sprite1->Draw();
+	sprite2->Draw();
 
 	// デバッグテキストの描画
 	debugText.DrawAll(cmdList);
