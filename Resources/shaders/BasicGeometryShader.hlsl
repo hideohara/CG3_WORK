a95225e2ad6531b969c4a34e610b963adb602de1
oharakogakuin <ohara@g.neec.ac.jp>
2022-10-06 17:08:06 +0900

    02_02.ジオメトリの加工

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,23 +1,34 @@
 #include "BasicShaderHeader.hlsli"
 
-//struct GSOutput
-//{
-//	float4 pos : SV_POSITION;
-//};
 
-[maxvertexcount(3)]
+// 三角形の入力から、三角形を2つ出力
+[maxvertexcount(6)]
 void main(
-	triangle VSOutput  input[3] : SV_POSITION,
+	triangle VSOutput input[3] : SV_POSITION,
 	inout TriangleStream< GSOutput > output
 )
 {
-	for (uint i = 0; i < 3; i++)
-	{
+	// 1つ目の三角形
+	for (uint i = 0; i < 3; i++) {
 		GSOutput element;
 		element.svpos = input[i].svpos;
 		element.normal = input[i].normal;
 		element.uv = input[i].uv;
 		output.Append(element);
 	}
+	// 現在のストリップを終了
+	output.RestartStrip();
+
+	// 2つ目の三角形
+	for (uint i = 0; i < 3; i++) {
+		GSOutput element;
+		// X方向に20ずらす
+		element.svpos = input[i].svpos + float4(20.0f, 0.0f, 0.0f, 0.0f);
+		element.normal = input[i].normal;
+		// UVを5倍に
+		element.uv = input[i].uv * 5.0f;
+		output.Append(element);
+	}
 }
+
 
